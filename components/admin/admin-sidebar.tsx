87ed4d08fc87651649a0c9508865895db096bfef
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2026-03-10 06:55:41 +0500

    admin side bar chnage with property registration module complete

--- components/admin/admin-sidebar.tsx
+++ components/admin/admin-sidebar.tsx
@@ -52,7 +52,7 @@ export function AdminSidebar() {
                     })
                 }
             })
-            .catch(() => {})
+            .catch(() => { })
     }, [])
     return (
         <>
